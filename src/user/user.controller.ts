e47d594a181a4ec0e5de851e3db5e61dacaf5713
Jagan S <jaganbecse17@gmail.com>
2025-11-02 00:04:08 +0530

    user get and save

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Post()
+  saveUser(@Body() data) {
+    return this.userService.saveUser(data)
+  }
+  
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+private   Users = [
+    {
+      id: 1,
+      name: 'Earth',
+      email: 'earth@milkyway.org',
+    },
+  ];
+
+  getAllUsers() {
+    return this.Users;
+  }
+
+  saveUser(user){
+    this.Users.push({...user,id:this.Users.length+1})
+  }
+}
